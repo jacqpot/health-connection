6038f9a7ce9db8147e81042e98b2cf9c0e28dc71
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-07-20 21:38:33 -0600

    adding edu new get and post for new and view

--- app/controllers/edu_controller.rb
+++ app/controllers/edu_controller.rb
@@ -8,4 +8,19 @@ class EduController < ApplicationController
         erb :'/edu/new'
     end
 
+    post '/edu/new' do 
+        @edu = current_provider.edu.create(title: params[:title], content: params[:content])
+        redirect to :'/edu/index'
+    end
+
+    get '/edu/:id' do 
+        @edu = Edu.find(params[:id])
+        if @edu.user == current_provider
+            erb :'/edu/view'
+        else
+            redirect to '/provider/view'
+        end
+    end
+    
+
 end
