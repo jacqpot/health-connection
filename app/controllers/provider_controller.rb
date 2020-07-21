49948fa2f26d5ed005ce721d7ec041a078a56afc
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-07-20 21:08:11 -0600

    created view pages and first three get methods

--- app/controllers/provider_controller.rb
+++ app/controllers/provider_controller.rb
@@ -1,7 +1,13 @@
 class ProviderController < ApplicationController 
 
-    get '/provider' do 
-        erb :'/provider/index'
+    get '/provider/signup' do
+        erb :"/provider/new"
     end
-
+    get '/provider/login' do 
+        erb :'/provider/login'
+    end
+    get '/provider/homepage' do 
+        @provider = Provider.find_by_id(session[user_id])
+        erb :'/provider/view'
+    end
 end
