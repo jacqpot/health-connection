caa2acc80c8ec75f43bd631531f39c537533c4b6
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-07-20 21:48:18 -0600

    finished edu controller. building views of edu next.

--- app/controllers/edu_controller.rb
+++ app/controllers/edu_controller.rb
@@ -21,6 +21,29 @@ class EduController < ApplicationController
             redirect to '/provider/view'
         end
     end
-    
+    get '/edu/:id/edit' do 
+        if logged_in_p? 
+            @edu = Edu.find(params[:id])
+            erb :'/edu/edit'
+        else
+            erb :'/provider/index'
+
+        end
+    end
+
+    patch '/edu/:id' do 
+        @edu = Edu.find(params[:id])
+        @edu.content = (params[:content])
+        if @edu.save
+            redirect to "/edu/#{edu.id}"
+        else
+            redirect "edu/#{edu.id}/edit"
+        end
+    end
+    delete "/edu/:id/delete" do 
+        edu = Post.find(params[:id])
+        edu.destroy
+        redirect "/edu/index"
+    end
 
 end
