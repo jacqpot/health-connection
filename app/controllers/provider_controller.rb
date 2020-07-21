9cb69d00cbc56fed07d5caa17ed1d75381be0433
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-07-20 21:20:35 -0600

    finished writing provider controller

--- app/controllers/provider_controller.rb
+++ app/controllers/provider_controller.rb
@@ -9,5 +9,22 @@ class ProviderController < ApplicationController
     get '/provider/homepage' do 
         @provider = Provider.find_by_id(session[user_id])
         erb :'/provider/view'
+
+    end
+    post "provider/login" do 
+        @provider = Provider.find_by(:name => params[:name])
+        if @provider && @provider.authenticate(params[:password])
+            session[:provider_id] = @provider.id
+            @session = session
+            erb :'/provider/view'
+        end
+    end
+    post '/provider/signup' do 
+        @provider = Provider.create(name: params[:name], title: params[:title], password: params[:password])
+        erb :'/provider/login'
+    end
+    get '/provider/logout' do 
+        session.clear
+        redirect '/'
     end
 end
