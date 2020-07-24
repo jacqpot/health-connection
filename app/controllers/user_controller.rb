68cd7403c6d792f2422b29f59b33066f1e34e8db
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-07-24 13:25:40 -0600

    fixed homepage error

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -20,8 +20,7 @@ class UserController < ApplicationController
     end
     get '/user/homepage' do 
         check_login
-
-        @user = User.find_by_id(session[user_id])
+        @user = User.find_by_id(session[:user_id])
         erb :'/user/view'
     end
 
