04fa8d2b80e8cd986225768b68556ed45cd660d4
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-07-26 14:41:52 -0600

    filled out apointments migration table

--- db/migrate/20200726203732_create_apointments.rb
+++ db/migrate/20200726203732_create_apointments.rb
@@ -1,7 +1,9 @@
 class CreateApointments < ActiveRecord::Migration
   def change
     create_table :apointments do |t|
-
+      t.string :date_and_time 
+      t.string :reason
+      t.integer :user_id
       t.timestamps null: false
     end
   end
